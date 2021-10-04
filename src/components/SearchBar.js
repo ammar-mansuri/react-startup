2155542664a53b59c9b6b6e27416faefa02c0ebe
Ammar Mansuri <AmmarMansuri@MacBook-Pro.local>
2021-10-04 22:33:31 +0200

    New Search Application

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+
+const SearchBar=()=>{
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="Search..."></input>
+            </form>
+        </div>
+    );
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import App from './components/App';
+
+    
+ReactDOM.render(<App/>,document.querySelector('#root'));
+
+// ReactDOM.render(<AppClassComponent/>,document.querySelector('#root'));
+
+//ReactDOM.render(<Clock/>,document.querySelector('#root'));
